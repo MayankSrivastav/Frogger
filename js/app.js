3e718780c26fd1ed40535a13381759c6d8d5e06d
MayankSrivastav <mayanksri18@yahoo.in>
2017-02-11 12:10:18 +0530

    Fixed the player's movement code. Also, updated collision detection code.

--- js/app.js
+++ js/app.js
@@ -1,101 +1,117 @@
 // Enemies our player must avoid
 var Enemy = function(x, y, speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
     this.x = x;
     this.y = y;
-    this.speed = speed;
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+    this.speed = speed;   
     this.sprite = 'images/enemy-bug.png';
 };
 
-// Update the enemy's position, required method for game
+// Update the enemy's position
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
+    // Multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
     if (this.x > 505) {
-        this.x -= 505; 
+        this.x -= 505;
     }
-    this.x += this.speed * dt * 4 ;    
+    this.x += this.speed * dt * 3;
 };
 
-// Draw the enemy on the screen, required method for game
+// Draw the enemy on the screen
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-var Player = function() {
+// Player class
+var Player = function(x, y, speed) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
     this.sprite = "images/char-boy.png";
-    this.x = 200;
-    this.y = 400;
 };
+
 // This class requires an update(), render() and
 // a handleInput() method.
 Player.prototype.update = function() {
     if (this.y === 0) {
-        alert("You Win!!!");
-        this.x = 200;
-        this.y = 400;
+        console.log("You Win!!!");
+        this.x = 202.5;
+        this.y = 383;
     } else {
         this.checkCollisions(allEnemies);
     }
 };
 
+// This function checks if collision has occured
+// between enemy & player. If so, then game is
+// over and player is reset to its originial
+// position
 Player.prototype.checkCollisions = function(enemies) {
     if (this.y !== 0) {
         for (var i = 0; i < enemies.length; ++i) {
-            if (enemies[i].x + 20 <= this.x && enemies[i].y + 20 <= this.y) {
-                alert("Game over :()");
-                this.x = 200;
-                this.y = 400;
+            var thisEnemey = enemies[i];
+            if (player.x <= thisEnemey.x + 40 &&
+                player.y <= thisEnemey.y + 40 &&
+                thisEnemey.x <= player.x + 40 &&
+                thisEnemey.y <= player.y + 40) {
+                console.log("Game over :(");
+                this.x = 202.5;
+                this.y = 383;
             }
         }
     }
 };
 
+// This function renders the player on the
+// canvas
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+// This function handles the key inputs &
+// accordingly moves the player on the canvas
 Player.prototype.handleInput = function(keyCode) {
-    if (keyCode === 'up') { 
-        if (this.y - 50 > 0) {
-            this.y -= 50;
+    if (keyCode === 'up') {
+        if (this.y - this.speed > 0) {
+            this.y -= this.speed;
         } else {
             this.y = 0;
-        }      
-    } else if (keyCode === 'down') { 
-        if (this.y + 50 < 400) {
-            this.y += 50;
+        }
+    } else if (keyCode === 'down') {
+        if (this.y + this.speed < 383) {
+            this.y += this.speed;
         } else {
-            this.y = 400;
-        } 
-    } else if (keyCode === 'left') { 
-        if (this.x - 50 > 0) {
-            this.x -= 50;
+            this.y = 383;
+        }
+    } else if (keyCode === 'left') {
+        if (this.x - this.speed > 0) {
+            this.x -= this.speed;
         } else {
             this.x = 0;
         }
-    } else if (keyCode === 'right' && (this.x + 50 < 450)) {
-        this.x += 50;
+    } else if (keyCode === 'right') {
+        if (this.x + this.speed < 400) {
+            this.x += 50;
+        } else {
+            this.x = 400;
+        }
     }
 };
 
-// Now instantiate your objects.
-var enemy1 = new Enemy(10, 20, Math.random() * 200 / 2);
-var enemy2 = new Enemy(100, 150, Math.random() * 180 / 2);
-// Place all enemy objects in an array called allEnemies
-var allEnemies = [ enemy1, enemy2];
+// Instantiate and push all enemy objects
+// in an array allEnemies
+var allEnemies = [];
+allEnemies.push(new Enemy(10, 20, Math.random() * 200 / 2),
+                new Enemy(100, 150, Math.random() * 180 / 2),
+                new Enemy(60, 80, Math.random() * 150 / 2));
+
 // Place the player object in a variable called player
-var player = new Player();
+var player = new Player(202.5, 383, 50);
 
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// This listens for key presses and sends the
+// keys to the Player.handleInput() method.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
